9bdf3d43c765581861fdb2c61ce7aeaac11f313b
bitflame <monazzam@protonmail.com>
2021-12-06 15:21:43 -0500

    Added code to build a symbol table of Bag<String> and build a digraph with the same number of vertices

--- src/main/java/WordNet.java
+++ src/main/java/WordNet.java
@@ -1,35 +1,76 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.*;
 
 public class WordNet {
+    Queue<String> nouns;
+    BST<Integer, Bag<String>> ST;
+    int size;
+    boolean hasCycle = false;
+
     // constructor takes the name of two input files
     public WordNet(String synsets, String hypernyms) {
+        createDb(synsets);
+        createGraph(hypernyms);
+    }
+
+    private void createDb(String synsets) {
         In in = new In(synsets);
-        int index;
+        int index = 0;
         String synset;
-        System.out.printf("Index------Synset\n");
+        nouns = new Queue<>();
+        ST = new BST<>();
         while (in.hasNextLine()) {
             String[] a = in.readLine().split(",");
             index = Integer.parseInt(a[0]);
-            System.out.print("   "+index+"         ");
             synset = a[1];
             String[] syns = synset.split(" ");
-            for (String s:syns) {
-                System.out.print(" "+s);
+            for (String s : syns) {
+                nouns.enqueue(s);
+                if (ST.contains(index)) {
+                    Bag b = ST.get(index);
+                    b.add(s);
+                    ST.put(index, b);
+                } else {
+                    Bag b = new Bag();
+                    b.add(s);
+                    ST.put(index, b);
+                }
             }
-            System.out.println();
+        }
+        size = ST.size();
+    }
+
+    private void createGraph(String hypernyms) {
+        In in = new In(hypernyms);
+        Digraph digraph = new Digraph(size);/* assuming the number of ids are the same in synsets and hypernyms and I
+        believe it is the number of vertices in the graph */
+        int index = 0;
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(",");
+            // index is the id
+            for (int i = 0; i < a.length - 1; i++) {
+                digraph.addEdge(Integer.parseInt(a[i]), Integer.parseInt(a[i + 1]));
+            }
+        }// check for cycles
+        DirectedCycle cycleFinder = new DirectedCycle(digraph);
+        if (cycleFinder.hasCycle()) {
+            hasCycle = true;
+            return;
         }
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        Queue<String> nouns = new Queue<>();
         return nouns;
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        return true; // for now
+        for (int i : ST.keys()) {
+            for (String s : ST.get(i)) {
+                if (s.equals(word)) return true;
+            }
+        }
+        return false;
     }
 
     // distance between nounA and nounB (defined below )
